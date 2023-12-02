61a3a28819603d0cc8674626c1291c106f90da18
e01627753 <b-weber@gmx.at>
2023-12-02 22:20:52 +0100

    prepared login, user and post endpoints

--- BackEnd/social-hub/server/models.py
+++ BackEnd/social-hub/server/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+class Post(models.Model):
+    post_id = models.IntegerField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # TODO: user: { id  + username }
+    text = models.TextField()
+    #image = models.ImageField(upload_to='post_images') # TODO: image = url
