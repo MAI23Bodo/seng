20baa23af090e5ef70ac71f8bd3dc3a6100a38fe
e01627753 <b-weber@gmx.at>
2024-01-11 13:48:27 +0100

    fixed authentication and settings

--- BackEnd/social-hub/social_hub/settings.py
+++ BackEnd/social-hub/social_hub/settings.py
@@ -88,27 +88,6 @@ CORS_ALLOW_HEADERS = ['*']
 ALLOWED_HOSTS=['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
-
-
-
-
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    'http://localhost:3000',
-]
-
-
-CORS_ALLOW_HEADERS = ['*']
-ALLOWED_HOSTS=['*']
-CORS_ORIGIN_ALLOW_ALL = True
-
-
-
-
-
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
